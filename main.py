4cda55f94ed0671d37f703e01916da30e1e92a41
agnel <agnelvincent779@gmail.com>
2026-05-27 00:48:44 +0530

    implemented Euclidean formula as function

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import json
+import math
 
 with open('data.json', 'r') as file:
     data = json.load(file)
 
 print(data)
+
+def calculate_distance(c1, c2):
+    return math.sqrt((c2[0] - c1[0]) ** 2 + (c2[1] - c1[1]) ** 2)
+
