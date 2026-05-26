8ee1e8d7f1919e1583b15c32884b0da2653b268b
agnel <agnelvincent779@gmail.com>
2026-05-27 01:41:23 +0530

    added logic for finding the nearest agent for delivery

--- main.py
+++ main.py
@@ -9,3 +9,18 @@ print(data)
 def calculate_distance(c1, c2):
     return math.sqrt((c2[0] - c1[0]) ** 2 + (c2[1] - c1[1]) ** 2)
 
+
+agent_report = {}
+
+for package in data['packages']:
+    distances = {}
+    for agent in data['agents']:
+        distances[agent] = calculate_distance(data['warehouses'][package['warehouse']], data['agents'][agent])
+    nearest_agent = min(distances,key = lambda i : distances[i])
+    if not agent_report.get(nearest_agent):
+        agent_report[nearest_agent] = {'packages_delivered' : 1, 
+                                       }
+    else:
+        agent_report[nearest_agent]['packages_delivered'] += 1
+    
+print(agent_report)
