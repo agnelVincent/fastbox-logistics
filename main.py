e58bb75404480a45f50ce974c025e98485857600
agnel <agnelvincent779@gmail.com>
2026-05-27 09:50:27 +0530

    initialized empty dictionary for agent report for optimizing code

--- main.py
+++ main.py
@@ -7,20 +7,22 @@ with open('data.json', 'r') as file:
 def calculate_distance(c1, c2):
     return math.sqrt((c2[0] - c1[0]) ** 2 + (c2[1] - c1[1]) ** 2)
 
-agent_report = {}
+agent_report = {
+    agent : {
+        'packages_delivered' : 0,
+        'total_distance' : 0
+    } for agent in data['agents']
+}
+
 
 for package in data['packages']:
     distances = {}
     for agent in data['agents']:
         distances[agent] = calculate_distance(data['warehouses'][package['warehouse']], data['agents'][agent])
     nearest_agent = min(distances,key = lambda i : distances[i])
-    if not agent_report.get(nearest_agent):
-        agent_report[nearest_agent] = {'packages_delivered' : 1, 
-                                       'total_distance' : distances[nearest_agent] + calculate_distance(data['warehouses'][package['warehouse']],package['destination'])
-                                       }
-    else:
-        agent_report[nearest_agent]['packages_delivered'] += 1
-        agent_report[nearest_agent]['total_distance'] += distances[nearest_agent] + calculate_distance(data['warehouses'][package['warehouse']],package['destination'])
+
+    agent_report[nearest_agent]['packages_delivered'] += 1
+    agent_report[nearest_agent]['total_distance'] += distances[nearest_agent] + calculate_distance(data['warehouses'][package['warehouse']],package['destination'])
     
 
 for agent in agent_report:
