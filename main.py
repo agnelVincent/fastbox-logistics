75791e9a3c98fe0f01d80da419b4aa077ceb69a2
agnel <agnelvincent779@gmail.com>
2026-05-27 10:11:40 +0530

    created folder for checking test cases

--- main.py
+++ main.py
@@ -21,12 +21,13 @@ agent_report = {
 
 for package in data['packages']:
     distances = {}
+    warehouse_location = data['warehouses'][package['warehouse']]
     for agent in data['agents']:
-        distances[agent] = calculate_distance(data['warehouses'][package['warehouse']], data['agents'][agent])
+        distances[agent] = calculate_distance(warehouse_location, data['agents'][agent])
     nearest_agent = min(distances,key = lambda i : distances[i]) #Finding the nearest agent to the warehouse
 
     agent_report[nearest_agent]['packages_delivered'] += 1
-    agent_report[nearest_agent]['total_distance'] += distances[nearest_agent] + calculate_distance(data['warehouses'][package['warehouse']],package['destination'])
+    agent_report[nearest_agent]['total_distance'] += distances[nearest_agent] + calculate_distance(warehouse_location, package['destination'])
     #Summed the distance from warehouse to agent location with the distance from warehouse to destination
     
 
