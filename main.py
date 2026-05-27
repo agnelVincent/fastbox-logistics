93afba4e7ea2ba2519fabd27bb7140254038614d
agnel <agnelvincent779@gmail.com>
2026-05-27 09:43:36 +0530

    implemented total distance calculation and efficiency calculation logic

--- main.py
+++ main.py
@@ -4,11 +4,8 @@ import math
 with open('data.json', 'r') as file:
     data = json.load(file)
 
-print(data)
-
 def calculate_distance(c1, c2):
     return math.sqrt((c2[0] - c1[0]) ** 2 + (c2[1] - c1[1]) ** 2)
-
 
 agent_report = {}
 
@@ -19,8 +16,12 @@ for package in data['packages']:
     nearest_agent = min(distances,key = lambda i : distances[i])
     if not agent_report.get(nearest_agent):
         agent_report[nearest_agent] = {'packages_delivered' : 1, 
+                                       'total_distance' : distances[nearest_agent] + calculate_distance(data['warehouses'][package['warehouse']],package['destination'])
                                        }
     else:
         agent_report[nearest_agent]['packages_delivered'] += 1
+        agent_report[nearest_agent]['total_distance'] += distances[nearest_agent] + calculate_distance(data['warehouses'][package['warehouse']],package['destination'])
+    
+    agent_report[nearest_agent]['efficiency'] = agent_report[nearest_agent]['total_distance'] / agent_report[nearest_agent]['packages_delivered']
     
 print(agent_report)
