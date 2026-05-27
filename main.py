73319ded6c9ec5036a0baf03ccd0a8b40cbad992
agnel <agnelvincent779@gmail.com>
2026-05-27 10:06:56 +0530

    added comments in main.py

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ import math
 with open('data.json', 'r') as file:
     data = json.load(file)
 
+
+#Euclidean formula
 def calculate_distance(c1, c2):
     return math.sqrt((c2[0] - c1[0]) ** 2 + (c2[1] - c1[1]) ** 2)
 
+
+#Initializing empty agent report dictionary
 agent_report = {
     agent : {
         'packages_delivered' : 0,
@@ -19,12 +23,14 @@ for package in data['packages']:
     distances = {}
     for agent in data['agents']:
         distances[agent] = calculate_distance(data['warehouses'][package['warehouse']], data['agents'][agent])
-    nearest_agent = min(distances,key = lambda i : distances[i])
+    nearest_agent = min(distances,key = lambda i : distances[i]) #Finding the nearest agent to the warehouse
 
     agent_report[nearest_agent]['packages_delivered'] += 1
     agent_report[nearest_agent]['total_distance'] += distances[nearest_agent] + calculate_distance(data['warehouses'][package['warehouse']],package['destination'])
+    #Summed the distance from warehouse to agent location with the distance from warehouse to destination
     
 
+#Calculated the efficiency and rounded the figures
 for agent in agent_report:
     agent_report[agent]['total_distance'] = round(
         agent_report[agent]['total_distance'], 2
@@ -34,7 +40,7 @@ for agent in agent_report:
         agent_report[agent]['total_distance'] / agent_report[agent]['packages_delivered'], 2
     )
 
-
+#Found the best agent
 best_agent = min(
     agent_report,
     key = lambda agent : agent_report[agent]['efficiency']
@@ -45,5 +51,6 @@ best_agent = min(
 agent_report['best_agent'] = best_agent
 
 
+#Saved the agent report to report.json
 with open('report.json', 'w') as file:
     json.dump(agent_report,file, indent=4)
